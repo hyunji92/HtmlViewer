301e02d15cdba5290f7062ca70123fd2b23f5c95
Á¤ÇöÁö(chunghj09) <chunghj09@infobank.net>
2015-02-26 18:12:28 +0900

    Html viewer 브라우저 팝업으로 작업 중

---
+++ app/src/main/java/sample/info/htmlviewer/MainActivity.java
@@ -0,0 +1,81 @@
+package sample.info.htmlviewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+
+    WebView browser ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND,
+                WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
+        setContentView(R.layout.activity_main);
+
+        browser = (WebView) findViewById(R.id.webView);
+
+        browser.setWebViewClient(new myWebViewClient());
+        WebSettings webSettings = browser.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setBuiltInZoomControls(true);
+
+
+        //case1
+        //browser.loadUrl("http://www.naver.com");
+        //case2
+
+        browser.loadData("<meta http-equiv='Content-Type' content='text/html; charset=utf-8' /><html><body>Hello, Sample   !</body></html>", "text/html", "utf-8");
+        //case3
+        //browser.loadUrl("file:///android_asset/test.htm");
+
+        browser.getSettings().setJavaScriptEnabled(true);    // 자바스크립트 사용 시 설정
+
+
+    }
+    class myWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+            view.loadUrl(url);
+            return true;
+        }
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
